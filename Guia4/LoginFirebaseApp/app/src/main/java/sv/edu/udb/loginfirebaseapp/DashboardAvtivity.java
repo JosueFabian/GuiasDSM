82b30bd86f821825d749e196b9bf3e14c99dd27c
JosueFabian <alexander.rivas2014@outlook.com>
2022-05-14 21:01:55 -0600

    Agregando guias

---
+++ Guia4/LoginFirebaseApp/app/src/main/java/sv/edu/udb/loginfirebaseapp/DashboardAvtivity.java
@@ -0,0 +1,15 @@
+package sv.edu.udb.loginfirebaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_avtivity);
+
+    }
+}
